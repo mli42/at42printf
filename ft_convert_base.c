90419cbde19c9c8bfed0964b1bda5932415d338e
Marc LI <mli@e3r6p6.42.fr>
2019-11-15 14:33:33 +0100

    convert nb to hexadecimal

---
+++ ft_convert_base.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/12 23:33:41 by mli               #+#    #+#             */
+/*   Updated: 2019/11/15 14:32:32 by mli              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int		result_size(long int nb, long int base_len)
+{
+	int i;
+
+	i = 1;
+	while (nb >= base_len)
+	{
+		nb /= base_len;
+		i++;
+	}
+	return (i + 1);
+}
+
+void	ft_swap(char *r, int max)
+{
+	int i;
+	int swap;
+
+	i = 0;
+	while (i < max)
+	{
+		swap = r[i];
+		r[i++] = r[max];
+		r[max--] = swap;
+	}
+}
+
+char	*ft_convert(char *base, long int nbr)
+{
+	int			i;
+	int			size;
+	long int	base_len;
+	char		*result;
+
+	i = 0;
+	base_len = 16;
+	size = result_size(nbr, base_len);
+	if (!(result = (char *)malloc(sizeof(char) * size)))
+		return (NULL);
+	while (nbr >= base_len)
+	{
+		result[i++] = base[nbr % base_len];
+		nbr /= base_len;
+	}
+	result[i] = base[nbr % base_len];
+	result[i + 1] = '\0';
+	ft_swap(result, i);
+	return (result);
+}
