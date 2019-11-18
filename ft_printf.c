0c8829998c14ae23c03294d42e5f238aeab9d145
Marc LI <mli@e3r6p6.42.fr>
2019-11-18 17:35:07 +0100

    Starting to write...

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,49 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 16:09:39 by mli               #+#    #+#             */
-/*   Updated: 2019/11/16 20:30:12 by mli              ###   ########.fr       */
+/*   Updated: 2019/11/18 17:34:19 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+
+#include <stdio.h>
+
+
+int		ft_found(const char *str, int *i, int *to_return, va_list ap)
+{
+	(*i)++;
+
+	return (1);
+}
+
+
 int		ft_printf(const char *str, ...)
 {
-	va_list ap;
-	int	a;
+	int		i;
+	int		j;
+	int		to_return;
+	va_list	ap;
 
+	i = 0;
+	to_return = 0;
 	va_start(ap, str);
-	a = va_arg(ap, int);
+	while (str[i])
+	{
+		j = 0;
+		while (str[i] && str[i] != '%')
+		{
+			i++;
+			j++;
+			to_return++;
+		}
+		if (j)
+			write(1, &str[i - j], j);
+		if (str[i] == '%')
+			if (!(ft_found(str, &i, &to_return, ap)))
+				return (-1);
+	}
 	va_end(ap);
-	return (a);
+	return (to_return);
 }
---
+++ main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 17:46:50 by mli               #+#    #+#             */
+/*   Updated: 2019/11/18 17:33:20 by mli              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ft_printf.h"
+
+int		main(void)
+{
+	int returned;
+
+//	int a = 1;
+//	int *b = &a;
+
+	returned = ft_printf("12345%6789", 1500000);
+	printf("\nand returns: %d\n", returned);
+	return (0);
+}
