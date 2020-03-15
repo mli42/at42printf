62037fab138692e61e57e508f7cb7ce5d95dac5c
mli <mli@student.42.fr>
2020-03-15 16:22:51 +0100

    {fd} opti

--- ftpf_get_special.c
+++ ftpf_get_special.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 16:04:06 by mli               #+#    #+#             */
-/*   Updated: 2020/03/15 00:04:58 by mli              ###   ########.fr       */
+/*   Updated: 2020/03/15 13:46:10 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,9 @@ int		ftpf_get_special(const char *str, int *i, t_ftpf *ftpf, va_list ap)
 		k++;
 	if (k < 14)
 		ft_putstr_fd((char *)colors[k], ftpf->fd);
-	else if (ft_strncmp(&str[*i], "{fd}", 4) &&
-			ft_strncmp(&str[*i], "{fd=open}", 9))
-		return (0);
-	else if (!ft_strncmp(&str[*i], "{fd}", 4) &&
-			((ftpf->fd = va_arg(ap, int)) < 1))
-		return (0);
-	else if (!ft_strncmp(&str[*i], "{fd=open}", 9) &&
-			((ftpf->fd = open(va_arg(ap, char *), va_arg(ap, int))) < 1))
+	else if ((ft_strncmp(&str[*i], "{fd=open}", 9) ||
+		((ftpf->fd = open(va_arg(ap, char *), va_arg(ap, int))) < 1)) &&
+		(ft_strncmp(&str[*i], "{fd}", 4) || (ftpf->fd = va_arg(ap, int)) < 1))
 		return (0);
 	ftpf_end_special(str, i);
 	return (1);
